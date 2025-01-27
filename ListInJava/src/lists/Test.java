946732bed08fdff945233de515bb9546fd90749d
Aishwarya Mhetre <mhetreaishwarya07@gmail.com>
2025-01-27 07:32:21 +0530

    updated

--- ListInJava/src/lists/Test.java
+++ ListInJava/src/lists/Test.java
@@ -1,9 +1,10 @@
-package lists;
+ package lists;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Test {
-
+ 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
   int[] ints= {10,20,30,-10,30,40};
@@ -11,8 +12,21 @@ public class Test {
   ArrayList<Integer> list=new ArrayList<>();
   for(int i:ints) {
 	  list.add(i);
-  }
-		
+  } 
+	System.out.println("Displying list:"+list);	
+	System.out.println("Displying list using forEach");
+	for(int i:list)
+		System.out.println(i);
+	System.out.println("Doubling the values");
+ for(int i=0;i<list.size();i++) {
+	 if(list.get(i)>20)
+		 list.set(i,list.get(i)*2);
+	
+} System.out.println(list);
+Collections.sort(list );
+System.out.println("Sorted list:"+list);
+
 	}
+	
 
 }
